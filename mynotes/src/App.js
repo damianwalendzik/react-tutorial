f1ab565fee5144fc6642988f752245282279a701
Damian Walendzik <Damianwalendzik7@gmail.com>
2024-05-08 23:11:00 +0200

    Basic States and receiving data from backend

---
+++ mynotes/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Header from './components/Headers'
+import NotesListPage from './pages/NotesListPage';
+
+function App() {
+  return (
+    <div className="App">
+      My Project
+      <Header />
+      <NotesListPage />
+    </div>
+  );
+}
+
+export default App;
